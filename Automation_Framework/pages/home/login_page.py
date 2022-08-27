2446cd3ac916fd7a01f3a2afbac1512c1b9209bc
zohaibejaz3 <80754230+zohaibejaz3@users.noreply.github.com>
2022-08-27 09:43:49 +0500

    converted to framework for more flexibility

--- Automation_Framework/pages/home/login_page.py
+++ Automation_Framework/pages/home/login_page.py
@@ -2,19 +2,40 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPage:
+    _login_link = "ast-custom-button-link"
+    _email_field = ".//div[@class='form-group']/input[@id='email']"
+    _pass_field = ".//div[@class='form-group']/input[@id='password']"
+    _submit_btn = ".//input[@value='Login']"
 
     def __init__(self, driver):
         self.driver = driver
 
+    def get_login_link(self):
+        return self.driver.find_element(By.CLASS_NAME, self._login_link)
+
+    def get_email_field(self):
+        return self.driver.find_element(By.XPATH, self._email_field)
+
+    def get_pass_field(self):
+        return self.driver.find_element(By.XPATH, self._pass_field)
+
+    def get_submit_btn(self):
+        return self.driver.find_element(By.XPATH, self._submit_btn)
+
+    def click_login_link(self):
+        self.get_login_link().click()
+
+    def enter_email(self, email):
+        self.get_email_field().send_keys(email)
+
+    def enter_pass(self, password):
+        self.get_pass_field().send_keys(password)
+
+    def click_submit_btn(self):
+        self.get_submit_btn().click()
+
     def login(self, username, password):
-        login_link = self.driver.find_element(By.CLASS_NAME, "ast-custom-button-link")
-        login_link.click()
-
-        email_field = self.driver.find_element(By.XPATH, ".//div[@class='form-group']/input[@id='email']")
-        email_field.send_keys(username)
-
-        pass_field = self.driver.find_element(By.XPATH, ".//div[@class='form-group']/input[@id='password']")
-        pass_field.send_keys(password)
-
-        submit_btn = self.driver.find_element(By.XPATH, ".//input[@value='Login']")
-        submit_btn.click()
+        self.click_login_link()
+        self.enter_email(username)
+        self.enter_pass(password)
+        self.click_submit_btn()
