c88aa27cdd50b05352197ae51d6270d06f052610
zohaibejaz3 <80754230+zohaibejaz3@users.noreply.github.com>
2022-08-28 10:01:11 +0500

    Aligned to work with custom framework

--- Automation_Framework/pages/home/login_page.py
+++ Automation_Framework/pages/home/login_page.py
@@ -1,38 +1,40 @@
 from selenium.webdriver.common.by import By
+from base.selenium_driver import SeleniumDriver
 
 
-class LoginPage:
+class LoginPage(SeleniumDriver):
     _login_link = "ast-custom-button-link"
     _email_field = ".//div[@class='form-group']/input[@id='email']"
     _pass_field = ".//div[@class='form-group']/input[@id='password']"
     _submit_btn = ".//input[@value='Login']"
 
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
 
-    def get_login_link(self):
-        return self.driver.find_element(By.CLASS_NAME, self._login_link)
-
-    def get_email_field(self):
-        return self.driver.find_element(By.XPATH, self._email_field)
-
-    def get_pass_field(self):
-        return self.driver.find_element(By.XPATH, self._pass_field)
-
-    def get_submit_btn(self):
-        return self.driver.find_element(By.XPATH, self._submit_btn)
+    # def get_login_link(self):
+    #     return self.driver.find_element(By.CLASS_NAME, self._login_link)
+    #
+    # def get_email_field(self):
+    #     return self.driver.find_element(By.XPATH, self._email_field)
+    #
+    # def get_pass_field(self):
+    #     return self.driver.find_element(By.XPATH, self._pass_field)
+    #
+    # def get_submit_btn(self):
+    #     return self.driver.find_element(By.XPATH, self._submit_btn)
 
     def click_login_link(self):
-        self.get_login_link().click()
+        self.elementClick(self._login_link, locatorType="class")
 
     def enter_email(self, email):
-        self.get_email_field().send_keys(email)
+        self.sendKeys(email, self._email_field, locatorType="xpath")
 
     def enter_pass(self, password):
-        self.get_pass_field().send_keys(password)
+        self.sendKeys(password, self._pass_field, locatorType="xpath")
 
     def click_submit_btn(self):
-        self.get_submit_btn().click()
+        self.elementClick(self._submit_btn, locatorType="xpath")
 
     def login(self, username, password):
         self.click_login_link()
